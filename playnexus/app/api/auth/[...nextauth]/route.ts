28bc9542c13c1c2afaf0ec4cf19ed3ffb050b811
GouravSingh2005 <gouravsingh56466@gmail.com>
2025-03-29 14:49:55 +0530

    Add next-auth

---
+++ playnexus/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID ? "Loaded " : "Not Found ");
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET ? "Loaded " : "Not Found ");
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,  
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
